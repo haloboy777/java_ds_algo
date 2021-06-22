c51ece9c52fd42b2c982aacccc03dc0ea9e12584
Ayush Singh <ayush.singh@hyperdart.com>
2021-06-23 04:04:57 +0530

    AS - linked list stuff

---
+++ src/IntersectionOfLL.java
@@ -0,0 +1,52 @@
+public class IntersectionOfLL {
+
+  public static void main(String[] args) {
+    ListNode a = new ListNode(5), b = new ListNode(10);
+    int len_a = length(a);
+    int len_b = length(b);
+
+    int d = len_b - len_a;
+
+    if (len_a > len_b) {
+      d = len_a - len_b;
+      ListNode t = a;
+      a = b;
+      b = t;
+    }
+
+    while (d > 0) {
+      b = b.next;
+      d--;
+    }
+
+    while (a != null && b != null) {
+      if (a == b) {
+        System.out.println(a.val);
+        System.exit(0);
+      }
+      a = a.next;
+      b = b.next;
+    }
+    
+  }
+
+  static int length(ListNode x) {
+    int l = 0;
+    while (x != null) {
+      l++;
+      x = x.next;
+    }
+    return l;
+  }
+
+  static class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+}
---
+++ src/ReversingALinkedList.java
@@ -0,0 +1,75 @@
+public class ReversingALinkedList {
+
+  public static void main(String[] args) {
+    ListNode start = new ListNode(0);
+    ListNode x = start;
+    for (int i = 0; i < 10; i++) {
+      ListNode n = new ListNode(i + 1);
+      x.next = n;
+      x = n;
+    }
+    // start = reverseLL(start);
+    start = reverseLLWithRange(start, 3, 5);
+
+    System.out.println();
+    while (start != null) {
+      System.out.print(start.val + "  ->  ");
+      start = start.next;
+    }
+  }
+
+  static ListNode reverseLL(ListNode a) {
+    ListNode node = a;
+    ListNode prev = null;
+    while (node != null) {
+      ListNode next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    a = prev;
+    return a;
+  }
+
+  static ListNode reverseLLWithRange(ListNode a, int m, int n) {
+    if (m == n) return a;
+
+    ListNode start = null, start_prev = null;
+    ListNode end = null, end_next = null;
+
+    int i = 1;
+    ListNode curr = a;
+    while (curr != null && i <= n) {
+      if (i < m) start_prev = curr;
+
+      if (i == m) start = curr;
+
+      if (i == n) {
+        end = curr;
+        end_next = curr.next;
+      }
+
+      curr = curr.next;
+      i++;
+    }
+    end.next = null;
+
+    end = reverseLL(start);
+
+    if (start_prev != null) start_prev.next = end; else a = end;
+
+    start.next = end_next;
+    return a;
+  }
+
+  static class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+}
